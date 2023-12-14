292f8fa456bd09a9fb58e667ba0f03f9d8979d5a
nhphu19 <nhphu19@gmail.com>
2023-12-14 10:25:00 +0700

    [phu] change executable_path

--- selenium_automation.py
+++ selenium_automation.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path='D:/WolfSolutions/selenium/chromedriver.exe')
+service = Service(executable_path='./chromedriver.exe')
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
